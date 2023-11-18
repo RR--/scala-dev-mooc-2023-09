5b25262f2bcc19a1e2b94368172667aa041ebd6b
Ruslan <ruslan.ryumin@gmail.com>
2023-11-18 18:13:03 +0300

    tupleF

--- src/main/scala/module2/09-hkt.scala
+++ src/main/scala/module2/09-hkt.scala
@@ -12,7 +12,8 @@ object higher_kinded_types{
     a.flatMap{ a => b.map((a, _))}
 
 
-  def tuplef[F[_] : Bindable, A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
+  def tuplef[F[_], A, B](fa: F[A], fb: F[B])(implicit bnda: F[A] => Bindable[F, A], bndb: F[B] => Bindable[F, B]): F[(A, B)] =
+    fa.flatMap(a => fb.map(b => (a, b)))
 
 
   trait Bindable[F[_], A] {
@@ -23,13 +24,17 @@ object higher_kinded_types{
   def tupleBindable[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
     fa.flatMap(a => fb.map(b => (a, b)))
 
-  def optBindable[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+  implicit def optBindable[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
     override def map[B](f: A => B): Option[B] = opt.map(f)
 
     override def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
   }
 
-  def listBindable[A](list: List[A]): Bindable[List, A] = ???
+  implicit def listBindable[A](list: List[A]): Bindable[List, A] = new Bindable[List, A] {
+    override def map[B](f: A => B): List[B] = list.map(f)
+
+    override def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
+  }
 
 
 
