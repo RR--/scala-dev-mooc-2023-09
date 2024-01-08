b9bbc4d2da74b88e9711a7d5034e688d26eec274
Ruslan <ruslan.ryumin@gmail.com>
2024-01-08 17:36:54 +0300

    scala 3 monad parser

--- src/main/scala/scala3_1/CsvParser_homework.scala
+++ src/main/scala/scala3_1/CsvParser_homework.scala
@@ -3,7 +3,6 @@ package scala3_1
 
 
 
-/*
 //1. исполользовать given, как написано в комментариях и в почеченных местах ниже
 //2. использовать новый "тихий синтаксис", где сочтете приемлемым, тут на ваше усмотрение
 //https://docs.scala-lang.org/scala3/new-in-scala3.html  глава New & Shiny: The Syntax
@@ -37,17 +36,21 @@ trait FieldConversion[A,B]:
   def convert(x: A): B
 
 given intFieldConversion: FieldConversion[String,Int] with
-  def convert(x: String): Int = ???
+  def convert(x: String): Int = x.toInt
 // сделать given instance для типов Int Float Double
 // в функции просто сконвертнуть строку в нужный тип
+given floatFieldConversion: FieldConversion[String,Float] with
+  def convert(x: String): Float = x.toFloat
+
+given booleanFieldConversion: FieldConversion[String,Boolean] with
+  def convert(x: String): Boolean = x.toBoolean
 
 object TestExecution{
 
   //здесь написать функцию, которая будет применять given определенные выше
   // использовать using fieldConversion c первым параметром String, а второй будет вариативны параметр B
 
-  def parse[String,B](x:String)(?????????) : B =
-    ...вызвать собственнь функцию из трейта FieldConversion...
+  def parse[String,B](x:String)(using fc: FieldConversion[String, B]): B = fc.convert(x)
 
 
   def main(args: Array[String]): Unit = {
@@ -55,32 +58,32 @@ object TestExecution{
     def StringField =
       MonadParser[String, String] { str =>
         val idx = str.indexOf(";")
-        if (idx > -1)
+        if idx > -1 then
           (str.substring(0, idx), str.substring(idx + 1))
         else
           (str, "")
       }
 
-    def IntField =  ??? //StringField.map(...здесь применить parse который подхватит нужный given автоматически ...)
-    def FloatField = ???
-    def BooleanField =???
+    def IntField =  StringField.map(parse[String, Int])
+    def FloatField = StringField.map(parse[String, Float])
+    def BooleanField = StringField.map(parse[String, Boolean])
 
     case class Car(year: Int, mark: String, model: String, comment: String, price: Float)
 
     val str = "1997;Ford;E350;ac, abs, moon;3000\n1996; Jeep; Grand Cherokee; MUST SELL! air, moon roof, loaded; 4799"
 
     val parser =
-      for {
+      for
         year <- IntField
         mark <- StringField
         model <- StringField
         comment <- StringField
         price <- FloatField
-      } yield Car(year, mark, model, comment, price)
+      yield Car(year, mark, model, comment, price)
 
 
     val result = str.split('\n').map(parser.parse)
 
     println(result.map(x=>s"${x.model},${x.mark},${x.year}").mkString(";"))
   }
-}*/
+}
